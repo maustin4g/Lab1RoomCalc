44f76e2ecff588124976452b7d241fb565b9705e
Millicent Austin <maustin4g@gmail.com>
2017-01-31 17:07:43 -0500

    Final version

--- src/RoomCalc.java
+++ src/RoomCalc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-import static javafx.scene.input.KeyCode.Y;
+//import static javafx.scene.input.KeyCode.Y;
 
 /**
  * Created by Millicent on 1/30/2017.
@@ -11,37 +11,48 @@ public class RoomCalc {
         Scanner scan = new Scanner(System.in);
 
         //2. output so the user knows what's up
-        System.out.println("Welcome to the Room Perimeter Calculator");
+        System.out.println("Welcome to the Room Calculator");
+int choose = 1;
+        while (choose==1) {
+            //3. ask for, and capture user input
+            System.out.print("Please enter height of perimeter: ");
+            double length = scan.nextDouble();
 
-        //3. ask for input
-        System.out.print("Please enter height of perimeter: ");
-        double length = scan.nextDouble();
-
-        System.out.print("Please enter width of perimeter: ");
-        double width = scan.nextDouble();
+            System.out.print("Please enter width of perimeter: ");
+            double width = scan.nextDouble();
 
 
-        //4. perform calculations
-        double perimeter = (2 *length) + (2 * width);
-        double area = length * width;
+            //4. perform calculations
+            double perimeter = (2 * length) + (2 * width);
+            double area = length * width;
 
-        //5. output results
-        System.out.println("The perimeter is " + perimeter);
-        System.out.println("The area is " + area);
+            //5. output results
+            System.out.println("The perimeter is " + perimeter);
+            System.out.println("The area is " + area);
+            System.out.println("Do you want to continue? Enter 1 for yes, or 2 for No");
+            choose = scan.nextInt();
+        }
 
+
+
+                System.out.println("Thank you, have a nice day!");
+
+            }
+
+        }
+
+        //6. Declare and initialize variable
+       // int count = 0;
+       // int answer = 1;
+
+        //7. request and capture user input
+       // System.out.print("Would you like to continue? If yes enter 1, if no enter 2");
+       // int answer = scan.nextInt();
+
+        //8. Use conditional logic to create a boolean
 
         //6. close out resources
-        scan.close();
+        //scan.close();
 
-        System.out.println();
-        System.out.println("Length: " + length);
-        System.out.println("Width: " + width);
-        System.out.println();
-        System.out.println("Perimeter: " + perimeter);
-        System.out.println("Area: " + area);
 
-        BooleanFlag;
-        for(again = Y; i<11; i++){
-            System.out.println("Count is: " + i)
-    }
-}
+
